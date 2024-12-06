f2142b74628a2c4921b6539b4caf58406f927b46
Juan <38849891+xoltia@users.noreply.github.com>
2024-12-06 15:13:54 -0600

    Remove gg dependency

--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/BurntSushi/toml v1.4.0
 	github.com/dgraph-io/badger/v4 v4.5.0
 	github.com/diamondburned/arikawa/v3 v3.4.0
-	github.com/fogleman/gg v1.3.0
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
 	github.com/kkdai/youtube/v2 v2.10.1
 	github.com/wader/goutubedl v0.0.0-20241204165758-63dcb4b7f53f
@@ -22,9 +21,7 @@ require (
 	github.com/dop251/goja v0.0.0-20241024094426-79f3a7efcdbd // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/go-sourcemap/sourcemap v2.1.4+incompatible // indirect
-	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
-	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/flatbuffers v24.3.25+incompatible // indirect
 	github.com/google/pprof v0.0.0-20241206021119-61a79c692802 // indirect
 	github.com/gorilla/schema v1.4.1 // indirect
@@ -37,5 +34,6 @@ require (
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 	golang.org/x/time v0.8.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.35.2 // indirect
 )
--- images.go
+++ images.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"image"
+	"image/color"
 	"image/draw"
 	"image/png"
 	"net/http"
@@ -15,9 +16,10 @@ import (
 	_ "image/gif"
 	_ "image/jpeg"
 
+	"golang.org/x/image/font"
+	"golang.org/x/image/math/fixed"
 	_ "golang.org/x/image/webp"
 
-	"github.com/fogleman/gg"
 	"github.com/golang/freetype"
 	"github.com/golang/freetype/truetype"
 	"github.com/xoltia/mdk3/queue"
@@ -142,7 +144,6 @@ func writePreviewPoster(
 	return previewPath, savePNG(previewPath, img)
 }
 
-// TODO: remove gg dependency
 func writeLoadingPoster(thumbnail image.Image) (string, error) {
 	poster, err := os.Create(loadingPath)
 	if err != nil {
@@ -151,24 +152,31 @@ func writeLoadingPoster(thumbnail image.Image) (string, error) {
 
 	defer poster.Close()
 
-	largeThumbnail := image.NewRGBA(image.Rect(0, 0, 1920, 1080))
-	xdraw.ApproxBiLinear.Scale(largeThumbnail, largeThumbnail.Bounds(), thumbnail, thumbnail.Bounds(), xdraw.Over, nil)
+	img := image.NewRGBA(image.Rect(0, 0, 1920, 1080))
+	xdraw.ApproxBiLinear.Scale(img, img.Bounds(), thumbnail, thumbnail.Bounds(), xdraw.Over, nil)
+	drawShadow(img, 0, 0, 1920, 1080, 160)
 
-	dc := gg.NewContext(1920, 1080)
-	dc.DrawImage(largeThumbnail, 0, 0)
-	dc.SetRGBA(0, 0, 0, 0.5)
-	dc.DrawRectangle(0, 0, 1920, 1080)
-	dc.Fill()
-
-	dc.SetRGB(1, 1, 1)
-	dc.SetFontFace(truetype.NewFace(notoSansFont, &truetype.Options{
+	face := truetype.NewFace(notoSansFont, &truetype.Options{
 		Size: 72,
 		DPI:  72,
-	}))
+	})
 
-	dc.DrawStringAnchored("Loading...", 960, 540, 0.5, 0.5)
+	drawer := font.Drawer{
+		Dst:  img,
+		Src:  image.White,
+		Face: face,
+	}
 
-	return loadingPath, dc.SavePNG(loadingPath)
+	text := "Loading..."
+	textWidth := drawer.MeasureString(text).Ceil()
+	drawer.Dot = fixed.P(960-textWidth/2, 540)
+	drawer.DrawString(text)
+	return loadingPath, savePNG(loadingPath, img)
+}
+
+func drawShadow(img *image.RGBA, x, y, w, h int, alpha uint8) {
+	shadow := image.NewUniform(color.RGBA{0, 0, 0, alpha})
+	draw.Draw(img, image.Rect(x, y, x+w, y+h), shadow, image.Point{}, draw.Over)
 }
 
 func savePNG(path string, img image.Image) error {
